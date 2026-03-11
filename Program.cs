004bdf907fde234afe822f991b21e5e3b53bcfa2
Osman Osmani <osman.osmani@gmail.com>
2026-03-11 16:26:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Skriv in produkter. Avsluta med att skriva 'exit'.");
+
+string[] products = new string[0];
+int index = 0;
+
+while (true)
+{
+    string input = Console.ReadLine();
+
+    if (input.ToLower().Trim() == "exit")
+    {
+        break;
+    }
+
+    Array.Resize(ref products, index + 1);
+    products[index] = input;
+    index++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Du angav följande produkter:");
+
+foreach (string product in products)
+{
+    Console.WriteLine("* " + product);
+}
+
+Console.ReadLine();
